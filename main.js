1a8427cb7fb28e909098b974d1f907e43f98855f
kevin202108 <88997988+kevin202108@users.noreply.github.com>
2022-09-25 16:54:15 +0800

    debug Js
    完善演算法 及 狀況排除

--- main.js
+++ main.js
@@ -1,72 +1,156 @@
+let isBlack = true;  //紀錄是否為黑棋下
+
 let reversi = {
-    stones : [],  // 以0儲存無子 1為黑 2為白 6為待翻黑 7為待翻白    //改成10x10的空間 最外圍一圈不顯示
+    stones : [],  // 以0儲存無子 1為黑 2為白 6為待翻黑 7為待翻白 11新下的黑 12新下的白    //改成10x10的空間 最外圍一圈不顯示
     init : function(){ 
         //將所有格子填入0 
         for(let r = 0; r < 10; r ++){
-            this.stones[i].push(Array(10));    //最外圍一圈也是
+            reversi.stones.push(Array(10));    //最外圍一圈也是
             for(let c = 0; c < 10; c ++){
-                this.stones[r][c] = 0;
+                reversi.stones[r][c] = 0;
             }
         }
         // 中間填入黑白
-        this.stones[3+1][3+1] = 1;
-        this.stones[4+1][4+1] = 1; 
-        this.stones[3+1][4+1] = 2;
-        this.stones[4+1][3+1] = 2;
+        reversi.stones[3+1][3+1] = 1;
+        reversi.stones[4+1][4+1] = 1; 
+        reversi.stones[3+1][4+1] = 2;
+        reversi.stones[4+1][3+1] = 2;
+        //更新棋盤
+        reversi.filp();
+        isBlack = true;
     },
     move : function(r,c){
-        let rInc2,cInc2;  //延伸檢查增加量
-        let isValiable = 5;     //紀錄先相同 還是先不同
-        for(let rInc = -1; rInc < 2; rInc ++){  //繞周圍一圈檢查
-            for(let cInc = -1; cInc < 2; cInc ++){
-                let cAround = c + cInc + 1;
-                let rAround = r + rInc + 1;
-                if(stones[rAround][cAround] != 0 && this.stones[r+1][c+1] != this.stones[rAround][cAround]){  //如果周圍有不ㄧ樣的棋 => 同向向外延伸檢查
-                    // 將不同的列為待翻
-                    let cCheck = c + this.cInc2 + 1;
-                    let rCheck = r + this.rInc2 + 1;
+        let rNow = r + 1;           //中心新下的那顆棋
+        let cNow = c + 1;
 
-                    for(let i = 0; i < 8; i ++){
-                        cInc2 += cInc;  //將Inc2＋Inc
-                        rInc2 += rInc;
-                        if(this.stones[rCheck][cCheck] != 0 && this.stones[rCheck][cCheck] != this.stones[r+1][c+1]){ //如果延伸有棋且不＝下的
-                            this.stones[rCheck][cCheck] += 5;  //將那顆列為待翻
-                            isValiable = 6;  //設為6 ＝> 先同後不同＝6 先不同後同＝18
-                        }else{  //如果相同  or  沒有棋              
-                            isValiable = isValiable * 3;
-                            break;  //跳出迴圈
-                        }   
-                    }
-                    // 確認是否可翻(相同之間夾著不同) 可＝>不做事 不可＝>改回來,呼叫notValible 最後＝>將全部棋盤待翻的翻
-                    if(isValiable != 18){ //不可以翻
-                        // 改回來
-                        for(let c = 0; c < 8; c ++){
-                            for(let r = 0; r < 8; r ++){  
-                                if(stones[r+1][c+1] == 6){  //不可翻黑改回白
-                                    stones = 2;
-                                }else if(stones[r+1][c+1] == 7){    //不可翻白改回黑
-                                    stones = 1;
-                                }
-                            }
-                        }
-                        notValibleMove();  //呼叫notValible動畫
-                    }
-                    //將全部待翻的翻
-                    for(let c = 0; c < 8; c ++){
-                        for(let r = 0; r < 8; r ++){  
-                            if(stones[r+1][c+1] == 6){  //待翻黑
-                                document.getElementById("stone"+r+c).style.backgroundColor = 'black';
-                                stones = 1; //改回正常紀錄
-                            }else if(stones[r+1][c+1] == 7){    //待翻白
-                                document.getElementById("stone"+r+c).style.backgroundColor = 'white';
-                                stones = 2;
-                            }
-                        }
-                    }                    
+        let isAround = false;       //周圍是否有棋
+        let canMove = false;        //判斷這能不能下
 
-                    this.cInc2 = 0;  //將Inc2重置
-                    this.rInc2 = 0;
-                    isValiable = 5;  //將記錄可否翻重置
+        let cAround = 0;            
+        let rAround = 0;    
+
+        let rCheck = rNow;             //延伸檢查格子的r,c
+        let cCheck = cNow;
+
+        let stripHasDif = false;         //紀錄延伸檢查那條是否經過不同的棋
+        let stripCanflip = false;        //紀錄延伸檢查那條是否可翻
+
+        //0.沒有棋才能下
+        if(reversi.stones[rNow][cNow] != 0){  
+            console.log("there has a stone 值:"+reversi.stones[rNow][cNow]);
+            return 0;
+        }
+
+        //1.把下的那格改成待翻
+        if(isBlack){
+            this.stones[rNow][cNow] = 11;  //黑的時間存新下黑
+        }else{
+            this.stones[rNow][cNow] = 12;  //白的時間存新下白
+        }
+        //2.繞周圍一圈
+        for(let rInc = -1; rInc < 2; rInc ++){  
+        for(let cInc = -1; cInc < 2; cInc ++){
+            rAround = r + rInc + 1;                                 //周圍要檢查棋的r,c
+            cAround = c + cInc + 1;  
+            if(rAround == rNow && cAround == cNow){                 //中間下的那顆棋不檢查
+                //console.log("中間不檢查")
+                continue;
+            }
+
+            //(2)檢查與自己不同的棋 => (有)向外延伸檢查,沿路改為待翻
+            if(reversi.stones[rAround][cAround] != 0 &&             //周圍的那格有棋 且與自己不同
+               reversi.stones[rNow][cNow]-10 != reversi.stones[rAround][cAround]){ 
+                
+                isAround = true;  //紀錄周圍有與自己不同的棋
+
+                //同向向外延伸檢查開始
+                for(let i = 0; i < 8; i ++){
+                    rCheck += rInc;
+                    cCheck += cInc;
+
+                    if(reversi.stones[rCheck][cCheck] == 0){
+                        break;
+                    }
+                    if(reversi.stones[rCheck][cCheck] != reversi.stones[rNow][cNow]-10){  
+                        //如果與自己不同
+                        stripHasDif = true;  //記錄起來
+                        reversi.stones[rCheck][cCheck] += 5;  //將他列為待翻
+
+                    }else{  
+                        //如果與自己相同
+                        if(stripHasDif){          //如果中間夾了不同 => 紀錄可翻
+                            stripCanflip = true;
+                            canMove = true;  
+                        }
+                        break;
+                    } 
+                    
+                }   //延伸檢查結束
+
+                if(stripCanflip){           // 如果這條可以翻
+                    reversi.filp();         //  翻
+                }else{                      // 不能翻
+                    this.canNotFlip();      //  改回來
+                }
+
+                stripHasDif = false;     //重設延伸檢查紀錄
+                stripCanflip = false;
+                
+                rCheck = rNow;             //延伸檢查格子的r,c
+                cCheck = cNow;
+            }
+        }}
+        
+        //(2)
+        if(!isAround || !canMove){  //周圍沒有與自己不同的棋 => 把自己改回,結束
+            reversi.stones[rNow][cNow] = 0;               // 改回無棋
+        }else{
+            if(isBlack){
+                document.getElementById("stone"+r+c).style.backgroundColor = 'black';
+                this.stones[rNow][cNow] = 1;  //黑的時間存新下黑
+                isBlack = false;
+            }else{
+                document.getElementById("stone"+r+c).style.backgroundColor = 'white';
+                this.stones[rNow][cNow] = 2;  //白的時間存新下白
+                isBlack = true;
+            }
+        }
+
+    },
+    canNotFlip : function(){                                // 將棋盤上所有待翻的棋改回來
+        for(let r = 1; r < 9; r ++){
+            for(let c = 1; c < 9; c ++){ 
+                if(reversi.stones[r][c] != 0){                  // 如果有棋
+                    if(reversi.stones[r][c] > 5 && reversi.stones[r][c] < 10){// 如果他是待翻的
+                        reversi.stones[r][c] -= 5;              // 改回原本顏色
+                    }
+                }
+            }
+        }
+    },
+    //將棋盤上所有待翻的棋翻面 並更新HTML
+    filp : function(){    
+        let rBoard = 0;
+        let cBoard = 0;  
+        for(let r = 1; r < 9; r ++){
+            for(let c = 1; c < 9; c ++){
+                rBoard = r - 1;
+                cBoard = c - 1;
+                //如果為待翻白,黑棋 => 翻成黑棋
+                if(reversi.stones[r][c] == 7 || reversi.stones[r][c] == 1){
+                    //黑棋顯示到html上
+                    document.getElementById("stone"+rBoard+cBoard).style.backgroundColor = 'black';
+                    reversi.stones[r][c] = 1;               //改回正常紀錄
+                //如果為待翻黑,白棋 => 翻成白棋
+                }else if(reversi.stones[r][c] == 6 || reversi.stones[r][c] == 2){
+
+                    //白棋顯示到html上
+                    document.getElementById("stone"+rBoard+cBoard).style.backgroundColor = 'white';
+                    reversi.stones[r][c] = 2;               //改回正常紀錄
+
+                }else if(reversi.stones[r][c] == 0){  
+                    //無棋顯示到html上
+                    document.getElementById("stone"+rBoard+cBoard).style.backgroundColor = 'rgb(182, 230, 200)';
                 }
             }
         }
@@ -76,28 +160,10 @@ let reversi = {
     }
 }
 
-reversi.init();
-
-function buttonOnClick(r,c){
-    reversi.move(r.c);
+function init(){
+    reversi.init();
 }
 
-
-
-
-
-
-
-
-
-// while(r < 8 && c < 8 && this.stones[r+this.rInc2][c+this.cInc2] != this.stones[r][c]){
-//     // 持續延伸，直到找到同色的棋 
-//     // 一直翻，翻到翻錯再翻回來。
-//     if(this.stones[][])             // 將中間不同顏色的棋翻成同色 若無同顏色的旗，則不可下?           
-
-
-
-//     this.rInc2 = rInc2 + rInc;
-//     this.cInc2 = cInc2 + cInc;
-   
-// }jhhj
+function stoneOnclick(r,c){
+    reversi.move(r,c);
+}
